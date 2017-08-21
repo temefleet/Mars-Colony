14c7f34008d341e57fdcb8e9aadf347267195d71
jefe <jefe@fleet.local>
2017-08-21 15:01:51 -0700

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { RegisterComponent } from './components/register/register.component';
+import { EncountersComponent } from './components/encounters/encounters.component';
+import { ReportComponent } from './components/report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RegisterComponent,
+    EncountersComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/encounters/encounters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encounters',
+  templateUrl: './encounters.component.html',
+  styles: []
+})
+export class EncountersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
