5e19001278c10bb9eda1caf5594c0039c80b182d
jefe <jefe@fleet.local>
2017-08-22 11:02:33 -0700

    Added alien service

---
+++ src/app/services/alien.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Alien } from '../models/alien';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class AlienService {
+  aliensUrl = 'https://red-wdp-api.herokuapp.com/api/mars/aliens';
+
+  constructor(private http: Http) {}
+
+  getAliens(): Promise<Alien[]> {
+    return this.http.get(this.aliensUrl)
+                    .toPromise()
+                    .then((response) => response.json().aliens())
+                    .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<string> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
